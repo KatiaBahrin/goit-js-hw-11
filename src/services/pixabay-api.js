2ccf4b14f8c678dcd569093c31f6549ccfb2b84c
Katia Bahrin <katia.b1101@gmail.com>
2023-11-05 18:59:09 +0100

    fix

--- src/services/pixabay-api.js
+++ src/services/pixabay-api.js
@@ -3,7 +3,7 @@ import { page } from '../index';
 
 export async function fetchPhotos(userRequest) {
     const BASE_URL = 'https://pixabay.com/api/';
-    const API_KEY = '39751708-ae241d73177d032978945eddf';
+    const API_KEY = '40490949-303ce2ac66f501020c02001ed';
     const params = new URLSearchParams({
         key: API_KEY,
         q: userRequest,
